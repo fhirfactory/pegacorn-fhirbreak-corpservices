b4db9613f607fe14c32db47cbddc622efe9ceae1
brendan.douglas@act.gov.au <brendan.douglas@act.gov.au>
2021-07-07 10:43:05 +1000

    Ldap connections.

---
+++ pegacorn-fhirbreak-corpservices-ldapscanner/src/main/java/net/fhirfactory/pegacorn/fhirbreak/ldapscanner/interact/beans/LdapScannerConnection.java
@@ -0,0 +1,87 @@
+package net.fhirfactory.pegacorn.fhirbreak.ldapscanner.interact.beans;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.directory.api.ldap.model.cursor.CursorException;
+import org.apache.directory.api.ldap.model.cursor.SearchCursor;
+import org.apache.directory.api.ldap.model.entry.Entry;
+import org.apache.directory.api.ldap.model.exception.LdapException;
+import org.apache.directory.api.ldap.model.message.Response;
+import org.apache.directory.api.ldap.model.message.SearchRequest;
+import org.apache.directory.api.ldap.model.message.SearchRequestImpl;
+import org.apache.directory.api.ldap.model.message.SearchResultEntry;
+import org.apache.directory.api.ldap.model.message.SearchScope;
+import org.apache.directory.api.ldap.model.name.Dn;
+import org.apache.directory.api.util.DateUtils;
+
+import net.fhirfactory.pegacorn.buildingblocks.apacheds.BaseLdapConnection;
+import net.fhirfactory.pegacorn.buildingblocks.datamodels.ldap.PractitionerLdapEntry;
+
+/**
+ * LDAP scanner.
+ * 
+ * @author Brendan Douglas
+ *
+ */
+public class LdapScannerConnection extends BaseLdapConnection {
+	
+	public LdapScannerConnection() throws LdapException {		
+		super(System.getenv("APACHEDS_HOST_NAME"), Integer.parseInt(System.getenv("APACHEDS_BASE_PORT")), true,  System.getenv("APACHEDS_CONNECT_NAME"), System.getenv("APACHEDS_CONNECT_CREDENTIAL"), "dc=practitioners,dc=com"); //TODO remove the dc=com from the partitition.
+	}
+
+	
+	/**
+	 * Returns a list of LDAP entries where the createTimestamp or the modifyTimestamp is >= the date supplied.
+	 * 
+	 * @param after
+	 * @param baseDN
+	 * @return
+	 * @throws LdapException
+	 * @throws CursorException
+	 * @throws IOException
+	 */
+	public List<PractitionerLdapEntry> search(Date after) throws LdapException, CursorException, IOException {	
+		
+		List<PractitionerLdapEntry>entries = new ArrayList<>();
+			
+		 // Create the SearchRequest object
+	    SearchRequest searchRequest = new SearchRequestImpl();
+	    searchRequest.setScope(SearchScope.ONELEVEL);
+	    searchRequest.addAttributes("*","+");
+	    searchRequest.setTimeLimit(0);
+	    searchRequest.setBase(new Dn(baseDN));
+	    searchRequest.setFilter("(|(createTimestamp >=" + DateUtils.getGeneralizedTime(after) +")(modifyTimestamp >=" + DateUtils.getGeneralizedTime(after) + "))");
+
+	    SearchCursor searchCursor = null;
+
+		try {
+			connect();
+			
+			searchCursor = connection.search(searchRequest);
+			
+		    while (searchCursor.next())
+		    {
+			
+		        Response response = searchCursor.get();
+				
+				if (response instanceof SearchResultEntry)
+		        {
+					Entry resultEntry = ((SearchResultEntry) response).getEntry();
+					
+					PractitionerLdapEntry practitionerLdapEntry = new PractitionerLdapEntry(baseDN, resultEntry);
+					
+					entries.add(practitionerLdapEntry);
+		        }
+		    }
+		} finally {
+			close();
+			
+			searchCursor.close();
+		}
+		
+		return entries;
+	}
+}
---
+++ pegacorn-fhirbreak-corpservices-ldapsync/src/main/java/net/fhirfactory/pegacorn/fhirbreak/ldapsync/interact/beans/LdapSyncConnection.java
@@ -0,0 +1,87 @@
+package net.fhirfactory.pegacorn.fhirbreak.ldapsync.interact.beans;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.directory.api.ldap.model.entry.DefaultEntry;
+import org.apache.directory.api.ldap.model.entry.DefaultModification;
+import org.apache.directory.api.ldap.model.entry.Entry;
+import org.apache.directory.api.ldap.model.entry.Modification;
+import org.apache.directory.api.ldap.model.entry.ModificationOperation;
+import org.apache.directory.api.ldap.model.exception.LdapException;
+
+import net.fhirfactory.pegacorn.buildingblocks.apacheds.BaseLdapConnection;
+import net.fhirfactory.pegacorn.buildingblocks.datamodels.ldap.LdapAttribute;
+import net.fhirfactory.pegacorn.buildingblocks.datamodels.ldap.LdapAttributeNameEnum;
+import net.fhirfactory.pegacorn.buildingblocks.datamodels.ldap.PractitionerLdapEntry;
+
+/**
+ * LDAP sync
+ * 
+ * @author Brendan Douglas
+ *
+ */
+public class LdapSyncConnection extends BaseLdapConnection {
+
+	public LdapSyncConnection() throws LdapException {
+		super(System.getenv("APACHEDS_HOST_NAME"), Integer.parseInt(System.getenv("APACHEDS_BASE_PORT")), true,  System.getenv("APACHEDS_CONNECT_NAME"), System.getenv("APACHEDS_CONNECT_CREDENTIAL"), "dc=practitioners,dc=com"); //TODO remove the dc=com from the partitition.
+	}
+
+	
+	/**
+	 * Add a practitioner entry.
+	 * 
+	 * @param newEntry
+	 * @throws LdapException
+	 */
+	public void addEntry(PractitionerLdapEntry newEntry) throws LdapException, IOException {
+				
+		try {
+			Entry entry = new DefaultEntry(newEntry.getDN(),
+		            "ObjectClass: 1.3.6.1.4.1.18060.17.2.5",
+		            "ObjectClass: top");
+			
+			for (Map.Entry<LdapAttributeNameEnum, LdapAttribute> attribute : newEntry.getAttributes().entrySet()) {
+		        entry.add(attribute.getKey().getName(), attribute.getValue().getValue());
+			}
+			
+			entry.add("cn", newEntry.getCommonName());
+			
+			connect();
+			
+			connection.add(entry);
+			
+		} finally {
+			close();
+		}
+	}
+
+	
+	/**
+	 * Modify a practitioner entry.
+	 * 
+	 * @param updatedEntry
+	 * @throws LdapException
+	 */
+	public void modifyEntry(PractitionerLdapEntry updatedEntry) throws LdapException, IOException {
+		List<Modification>modifications = new ArrayList<>();
+		
+		try {
+			
+			for (Map.Entry<LdapAttributeNameEnum, LdapAttribute> attribute : updatedEntry.getAttributes().entrySet()) {
+				modifications.add(new DefaultModification(ModificationOperation.REPLACE_ATTRIBUTE, attribute.getKey().getName(),attribute.getValue().getValue()));
+		    }
+			
+			connect();
+			
+			for (Modification modification : modifications) {
+				connection.modify(updatedEntry.getDN(), modification);
+			}
+
+		} finally {
+			close();
+		}
+	}
+}
